653de360c50ea578dd9ecb3ce198085070acdebb
kmiyamoto-eng <muryo40@gmail.com>
2021-05-06 22:47:21 +0900

    Q18からQ20を回答入力

--- task.rb
+++ task.rb
@@ -214,7 +214,21 @@ end
 
 class UserQ18
   # 以下に回答を記載
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
+  
+  def introduce
+  
+    if @age > 30
+      puts "こんにちは，#{@name}と申します。宜しくお願いいたします。"
 
+    else
+     puts "はいさいまいど〜，#{@name}です！！！"
+
+    end  
+  end
 end
 
 def q18
@@ -229,9 +243,12 @@ end
 class Item
   # 以下を修正して下さい
 
-  def initialize(name)
-    @name = name
-  end
+def initialize(name:)
+  @name = name
+end
+def name
+  @name
+end
 end
 
 def q19
@@ -241,15 +258,41 @@ def q19
 end
 
 class UserQ20
-  # 以下に回答を記載
-
+  # 以下に回答を記載 
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
+  def name
+    @name
+  end
+  def age
+    @age
+  end
 end
-
 class Zoo
   # 以下に回答を記載
+  def initialize(name:, entry_fee:)
+    @name = name
+    @entry_fee = entry_fee
+  end
 
+  def info_entry_fee(user)
+    case user.age
+      when 0..5
+      entry_fee = @entry_fee[:infant]
+      when 6..12
+      entry_fee = @entry_fee[:children]
+      when 13..64
+      entry_fee = @entry_fee[:adult]  
+      when 65..120
+      entry_fee =  @entry_fee[:senior]
+    end
+  
+    puts "#{user.name}さんの入場料金は#{entry_fee}です。"
+
+  end
 end
-
 
 def q20
   # ここは変更しないで下さい（動物園・ユーザー情報は変更していただいてOKです）
